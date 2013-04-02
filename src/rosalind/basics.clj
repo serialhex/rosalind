3dcf020e1e2e96045d1ea532f9cdee63949610f6
Justin Patera <serialhex@gmail.com>
2013-04-02 09:04:11 -0400

    refactoring

--- src/rosalind/basics.clj
+++ src/rosalind/basics.clj
@@ -3,19 +3,17 @@
 (defn dna-counter-fn
   "This actually does the counting"
   [dna cnt]
-  (loop [d dna
-         c cnt]
-    (let [l (first d)
-          r (rest d)]
-      (case l
-          \A (def h (assoc c :A (+ 1 (:A c))))
-          \G (def h (assoc c :G (+ 1 (:G c))))
-          \C (def h (assoc c :C (+ 1 (:C c))))
-          \T (def h (assoc c :T (+ 1 (:T c))))
-          nil)
-      (if (empty? r)
-        h
-        (recur r h)))))
+  (let [l (first dna)
+        r (rest dna)]
+    (case l
+        \A (def h (assoc cnt :A (+ 1 (:A cnt))))
+        \G (def h (assoc cnt :G (+ 1 (:G cnt))))
+        \C (def h (assoc cnt :C (+ 1 (:C cnt))))
+        \T (def h (assoc cnt :T (+ 1 (:T cnt))))
+        nil)
+    (if (empty? r)
+      h
+      (recur r h))))
 
 (defn dna-count
   "Counts DNA Neucleotides"
